5b12f13fd95f912c1e64a1f69c350d31e262146f
Oleg <grigorenko996@gmail.com>
2025-06-19 11:03:20 +0300

    canvas added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import LogoModel from "@/components/LogoModel";
 export default function Home() {
   return (
     <div
-      className="flex flex-col items-center justify-center min-h-screen"
+      className="flex flex-col items-center justify-center min-h-screen w-full border-red-500"
     >
       <LogoModel />
     </div>
--- src/components/LogoModel.tsx
+++ src/components/LogoModel.tsx
@@ -1,15 +1,32 @@
 'use client'
 
 import { useGLTF } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import * as THREE from 'three'
 
-export default function LogoModel() {
-    const { scene } = useGLTF("/models/hyperliquid-mesh-only2.glb");
+function Model() {
+    const { scene } = useGLTF('/models/hyperliquid-mesh-only.glb');
+
+    const ref = useRef<THREE.Group>(null)
+
+    useFrame(() => {
+      if (ref.current) {
+        ref.current.rotation.y += 0.01
+      }
+    })
+
     console.log('scene', scene);
 
+    return <primitive ref={ref} object={scene} />;
+}
+
+export default function LogoModel() {
     return (
-        <div>
-            <h1>LogoModel</h1>
-        </div>
+        <Canvas camera={{ position: [0, 0, 10] }}>
+            <ambientLight intensity={0.6} />
+            <directionalLight position={[5, 5, 5]} intensity={1} />
+            <Model />
+        </Canvas>
     );
 }
